b84ffcfe6b0f6cd7df2cf0cc69e8020bae117e13
Khofif Rohma Cahyani <148848929+Khofifrcc@users.noreply.github.com>
2026-05-22 00:11:35 +0300

    post api crud tamamlandı

--- megashop_backend/app/routers/post_router.py
+++ megashop_backend/app/routers/post_router.py
@@ -3,11 +3,25 @@ from app.schemas.post_schema import PostCreate
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
-posts = []
+posts = [
+    {
+        "id": 1,
+        "user_id": "user_1",
+        "caption": "New outfit today!",
+        "image": "https://picsum.photos/600"
+    }
+]
 
 @router.get("/")
 def get_posts():
     return posts
+
+@router.get("/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post["id"] == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post not found")
 
 @router.post("/")
 def create_post(post: PostCreate):
@@ -16,13 +30,28 @@ def create_post(post: PostCreate):
         **post.dict()
     }
     posts.append(new_post)
-    return new_post
+    return {
+        "message": "Post created successfully",
+        "data": new_post
+    }
+
+@router.put("/{post_id}")
+def update_post(post_id: int, updated_post: PostCreate):
+    for post in posts:
+        if post["id"] == post_id:
+            post.update(updated_post.dict())
+            return {
+                "message": "Post updated successfully",
+                "data": post
+            }
+    raise HTTPException(status_code=404, detail="Post not found")
 
 @router.delete("/{post_id}")
 def delete_post(post_id: int):
     for post in posts:
         if post["id"] == post_id:
             posts.remove(post)
-            return {"message": "Post deleted successfully"}
-
+            return {
+                "message": "Post deleted successfully"
+            }
     raise HTTPException(status_code=404, detail="Post not found")

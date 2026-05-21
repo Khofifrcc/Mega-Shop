cd75d856b132f17ad9b888843979d1d283556f4b
Khofif Rohma Cahyani <148848929+Khofifrcc@users.noreply.github.com>
2026-05-22 00:00:17 +0300

    add product post and reel models

---
+++ megashop_backend/app/models/post_model.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, id, user_id, caption, image):
+        self.id = id
+        self.user_id = user_id
+        self.caption = caption
+        self.image = image
---
+++ megashop_backend/app/models/reel_model.py
@@ -0,0 +1,7 @@
+class Reel:
+    def __init__(self, id, user_id, caption, video_url, product_id):
+        self.id = id
+        self.user_id = user_id
+        self.caption = caption
+        self.video_url = video_url
+        self.product_id = product_id
